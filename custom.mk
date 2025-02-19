b7930f3216365aaf2b6f8b8a4e0ee01114f53a82
Stefan Paul Noack <stefan@noack.fi>
2025-02-19 07:27:45 +0200

    make lcc build and run under Ubuntu 24.04.1 LTS Linux 5.15.167.4-microsoft-standard-WSL2

--- custom.mk
+++ custom.mk
@@ -1,1 +1,16 @@
+BUILDDIR:=$(shell pwd)/bin
+TARGET:=x86/linux
+CC:=$(CC) -m32
+CFLAGS:=$(CFLAGS) -DLCCDIR='"$(BUILDDIR)/"'
+HOSTFILE:=etc/linux.c
 
+custom:
+	rm -rvf ${BUILDDIR}
+	mkdir -vp ${BUILDDIR}/include
+	ln -vsf $$(pwd)/include/x86/linux/* ${BUILDDIR}/include
+	mkdir -vp ${BUILDDIR}/gcc
+	ln -vs $$(which cpp) ${BUILDDIR}/gcc/cpp
+	ln -vs /usr/lib/gcc/x86_64-linux-gnu/13/32/crtbegin.o ${BUILDDIR}/gcc
+	ln -vs /usr/lib/gcc/x86_64-linux-gnu/13/32/crtend.o ${BUILDDIR}/gcc
+	ln -vs /usr/lib/gcc/x86_64-linux-gnu/13/32/libgcc.a ${BUILDDIR}/gcc
+	make clean all test triple
